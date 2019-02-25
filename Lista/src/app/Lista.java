0e9b2455a95fd0a8a0622a1e81d7592901e01ff3
capanema <carvalho.capanema@gmail.com>
2019-02-25 18:48:03 -0300

    Exemplo Lista

---
+++ Lista/src/app/Lista.java
@@ -0,0 +1,62 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import func.Funcionarios;
+
+public class Lista {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantidade de funcionarios: ");
+		int n = sc.nextInt();
+		sc.hasNextLine();
+
+		List<Funcionarios> lista = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("\nFuncionario: " + (i + 1));
+
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			sc.nextLine();
+
+			System.out.println("Nome: ");
+			String nome = sc.nextLine();
+
+			System.out.println("Salario: ");
+			double salario = sc.nextDouble();
+
+			lista.add(new Funcionarios(id, nome, salario));
+		}
+
+		System.out.print("Digite o id do funcionario que tera o salario aumentado: ");
+		int id = sc.nextInt();
+
+		System.out.print("\nDigite a porcetangem de aumento: ");
+		double valor = sc.nextDouble();
+
+		for (Funcionarios x : lista) {
+
+			if (x.getId().equals(id)) {
+				x.aumentar(valor);
+
+			}
+		}
+
+		System.out.println("\nLista de empregados:");
+		for (Funcionarios x : lista) {
+
+			System.out.println(x);
+
+		}
+
+		sc.close();
+	}
+
+}
---
+++ Lista/src/func/Funcionarios.java
@@ -0,0 +1,53 @@
+package func;
+
+public class Funcionarios {
+
+	private Integer id;
+
+	private String nome;
+
+	private Double salario;
+
+	public Funcionarios(Integer id, String nome, Double salario) {
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public void aumentar(double valor) {
+
+		setSalario(salario + ((salario * valor) / 100));
+
+	}
+
+	public String toString() {
+
+		return id + ": " + nome + ", " + String.format("%.2f", salario);
+
+	}
+
+}
